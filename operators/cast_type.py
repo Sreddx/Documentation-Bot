3373632562ca0b87646096131ef848db25b25cff
Rahul Behal <137822232+rbehal1729@users.noreply.github.com>
2023-07-15 10:02:36 -0700

    Fix readjsonvalues+casttype for json string list (#52)

--- operators/cast_type.py
+++ operators/cast_type.py
@@ -9,12 +9,12 @@ class CastType(BaseOperator):
     @staticmethod
     def declare_name():
         return 'Cast Type'
-    
+
     @staticmethod
     def declare_category():
         return BaseOperator.OperatorCategory.MANIPULATE_DATA.value
-   
-    @staticmethod     
+
+    @staticmethod
     def declare_parameters():
         return [
             {
@@ -29,8 +29,8 @@ class CastType(BaseOperator):
                 "condition": "output_type == string[]"
             }
         ]
-    
-    @staticmethod    
+
+    @staticmethod
     def declare_inputs():
         return [
             {
@@ -38,8 +38,8 @@ class CastType(BaseOperator):
                 "data_type": "any",
             }
         ]
-    
-    @staticmethod    
+
+    @staticmethod
     def declare_outputs():
         return [
             {
@@ -55,41 +55,51 @@ class CastType(BaseOperator):
     ):
         input = ai_context.get_input('input', self)  # >_<
         input_type = ai_context.get_input_type('input', self)
+
         params = step['parameters']
+
         output_type = params.get('output_type')
-        is_comma_separated = params.get('is_comma_separated', 'false').lower() == 'true'
+        is_comma_separated = params.get('is_comma_separated')
+
         if input_type == "Document[]":
             if output_type == 'string':
-                # Document schema from langchain for reference: 
+                # Document schema from langchain for reference:
                 # https://github.com/hwchase17/langchain/blob/master/langchain/schema.py#L269
                 res = " ".join([d.page_content for d in input])
-                
+
                 ai_context.set_output('output', res, self)
-                return
         elif input_type == 'string':
             if output_type in ['[]', 'string[]']:
-                ai_context.set_output('output', self.best_effort_string_to_list(input, is_comma_separated), self)
-                return
+                output_list = self.best_effort_string_to_list(
+                    input, is_comma_separated)
+                ai_context.set_output('output', output_list, self)
         elif input_type == 'string[]':
             if output_type == 'string':
-                ai_context.set_output('output', ','.join(input), self)
-                return
+                output_string = ','.join(input)
+                ai_context.set_output('output', output_string, self)
+        else:
+            raise TypeError(
+                f'Dont know how to cast {input_type} to {output_type}')
 
-        raise TypeError(f'Dont know how to cast {input_type} to {output_type}')
-          
-            
     def best_effort_string_to_list(self, s, is_comma_separated):
         try:
             result = json.loads(s)
+            # If input is a JSON, return an array of the JSON string
             if isinstance(result, dict):
-                return [result]  # Wrap the dictionary in a list
+                return [json.dumps(result)]
+            # If this is an array of JSON objects or lists, stringify each item
+            # before returning the array. Final returned result will be an array of strings
             elif isinstance(result, list):
+                for i, item in enumerate(result):
+                    if isinstance(item, (list, dict)):
+                        result[i] = json.dumps(item)
+
                 return result
         except json.JSONDecodeError:
             pass
-        
-        if not is_comma_separated:
-            return [s]
 
-        result = s.split(',')
-        return [item.strip() for item in result]
+        if is_comma_separated:
+            result = s.split(',')
+            return [item.strip() for item in result]
+
+        return [s]
--- operators/read_json_values.py
+++ operators/read_json_values.py
@@ -3,11 +3,12 @@ import json
 from .base_operator import BaseOperator
 from ai_context import AiContext
 
+
 class ReadJsonValues(BaseOperator):
     @staticmethod
     def declare_name():
         return 'ReadJsonValues'
-    
+
     @staticmethod
     def declare_category():
         return BaseOperator.OperatorCategory.MANIPULATE_DATA.value
@@ -21,7 +22,7 @@ class ReadJsonValues(BaseOperator):
                 "placeholder": "Ex: 'key1,key2,key3'"
             }
         ]
-    
+
     @staticmethod
     def declare_allow_batch():
         return True
@@ -45,26 +46,34 @@ class ReadJsonValues(BaseOperator):
             }
         ]
 
-    def get_nested_values(self, json_object, keys):
+    def get_values_from_keys(self, json_object, keys):
+        """
+        Function to retrieve values from JSON object using provided keys.
+        Keys can include nested keys, separated by '.'. 
+        The retrieved value is converted to a string if not already.
+        """
         values = []
 
         for key in keys:
-            # If key is nested
-            if '.' in key:
-                nested_keys = key.split('.')
-                temp_json_object = json_object
+            temp_json_object = json_object
+            nested_keys = key.split('.')
 
-                # Traverse the json object using the nested keys
-                for nested_key in nested_keys:
-                    if nested_key in temp_json_object:
-                        temp_json_object = temp_json_object[nested_key]
-                
-                # Add the final value to the list
-                values.append(temp_json_object)
-            
-            # If key is not nested
-            elif key in json_object:
-                values.append(json_object[key])
+            for nested_key in nested_keys:
+                if nested_key in temp_json_object:
+                    temp_json_object = temp_json_object[nested_key]
+                else:
+                    # Break the loop if the key doesn't exist in the JSON object
+                    break
+            else:
+                # Only executed if the loop didn't encounter a 'break'
+
+                # Check if the result is a list or dict, and use json.dumps
+                if isinstance(temp_json_object, (list, dict)):
+                    values.append(json.dumps(temp_json_object))
+                else:
+                    # Convert to string if the result isn't a string
+                    values.append(str(temp_json_object) if not isinstance(
+                        temp_json_object, str) else temp_json_object)
 
         return values
 
@@ -74,18 +83,20 @@ class ReadJsonValues(BaseOperator):
         ai_context: AiContext
     ):
         json_string = ai_context.get_input('json_string', self)
+
         params = step['parameters']
         keys = params.get('keys').split(',')
 
         try:
             json_object = json.loads(json_string)
 
-            values = self.get_nested_values(json_object, keys)
-            
+            values = self.get_values_from_keys(json_object, keys)
+
             json_values = ', '.join(values)
 
             ai_context.add_to_log(f"Json values read: {json_values}")
             ai_context.set_output('json_values', json_values, self)
 
         except Exception as e:
-            ai_context.add_to_log(f"Failed to read JSON values. Error: {e}", color='red')
+            ai_context.add_to_log(
+                f"Failed to read JSON values. Error: {e}", color='red')
