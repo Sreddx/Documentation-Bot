5bb88909c5ed8f3a6321077e424fe2f7904236f2
MaxBrodeurUrbas <126629752+max-agenthub@users.noreply.github.com>
2023-08-11 18:55:42 -0700

    operators/github_merge_requester.py - commit created by https://agenthub.dev/pipeline?run_id=jpk9Cx9uRcy4xfcuRmpYEP (#92)

--- operators/github_merge_requester.py
+++ operators/github_merge_requester.py
@@ -14,6 +14,10 @@ class GitHubMergeRequester(BaseOperator):
         return 'Create GitHub merge requests'
 
     @staticmethod
+    def declare_description():
+        return 'This operator creates merge requests on GitHub.'
+
+    @staticmethod
     def declare_category():
         return BaseOperator.OperatorCategory.ACT.value
 
@@ -27,12 +31,14 @@ class GitHubMergeRequester(BaseOperator):
             {
                 "name": "repo_name",
                 "data_type": "string",
-                "placeholder": "user_name/repository_name"
+                "placeholder": "user_name/repository_name",
+                "description": "Enter the name of the repository in the format 'user_name/repository_name'."
             },
             {
                 "name": "branch",
                 "data_type": "string",
-                "placeholder": "Enter the branch (default is main)"
+                "placeholder": "Enter the branch (default is main)",
+                "description": "Enter the name of the branch to create the merge request on. If not specified, the default branch 'main' will be used."
             }
         ]
 
@@ -42,22 +48,26 @@ class GitHubMergeRequester(BaseOperator):
             {
                 "name": "list_of_filenames",
                 "data_type": "string[]",
+                "description": "A list of file names to be included in the merge request."
             },
             {
                 "name": "list_of_file_contents",
                 "data_type": "string[]",
+                "description": "A list of file contents corresponding to the file names."
             },
             {
                 "name": "pr_title",
                 "data_type": "string",
                 "optional": "1",
                 "placeholder": "Title for the Pull Request",
+                "description": "The title of the pull request. If not specified, a default title will be generated."
             },
             {
                 "name": "pr_description",
                 "data_type": "string",
                 "optional": "1",
                 "placeholder": "Description for the Pull Request",
+                "description": "The description of the pull request. If not specified, a default description will be generated."
             }
         ]
 
