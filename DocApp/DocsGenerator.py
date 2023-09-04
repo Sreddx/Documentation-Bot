ab992358410584601615a2193dff9a5fed65b7bc
Sreddx <rssebas08@outlook.com>
2023-09-04 12:47:11 -0600

    Added endpoints for checking regex and docs

--- DocApp/DocsGenerator.py
+++ DocApp/DocsGenerator.py
@@ -1,53 +1,80 @@
 from operators.ask_chatgpt import AskChatGpt
 from operators.github_file_read import GitHubFileReader
 from mock_ai_context import MockAiContext
-import openai
 
-# Create dictionary for repo_info
-repo_info = {
-    "parameters": {
-        "repo_name": "iNBest-cloud/Telematica_AI_0723",
-        "folders": "src",
-        "file_regex": ".*.tsx",
-        "branch": "develop"
-    },
-}
+def check_regex_with_repo(repo_name, folders, file_regex, branch):
+    # Create dictionary for repo_info
+    repo_info = {
+        "parameters": {
+            "repo_name": repo_name,
+            "folders": folders,
+            "file_regex": file_regex,
+            "branch": branch
+        },
+    }
 
-# Create dictionary for prompt info
-prompt_info = {
-    "parameters": {
-        "question": "Generate Markdown documentation for this code. This documentation is meant to summarize the purpose and technical details of this code file.Use headings to breakdown the documentation into the following sections: Summary: A one sentence summary of this component's functionality. Inputs: briefly describe the inputs and their purpose. Dependencies: briefly describe dependencies and their purpose. Outputs: briefly describe outputs and their purpose.",
-        "context": "",
-        "function": ""
-    },
-}
+    # Create mock ai context
+    test_ai_context = MockAiContext()
+    test_ai_context.set_input("context", "")
 
-# Create mock ai context
-test_ai_context = MockAiContext()
-test_ai_context.set_input("context", "")
+    # Get repo file names and contents into ai context
+    GitHubFileReader().run_step(repo_info, test_ai_context)
+    return test_ai_context.get_output("file_names")
 
-# Get repo file names and contents into ai context
-GitHubFileReader().run_step(repo_info, test_ai_context)
-print(test_ai_context.get_output("file_names"))
+    
 
-num_files_to_read = len(test_ai_context.get_output("file_names"))
-docs = {}
+    
 
-# Generate documentation for each file
-for i in range(num_files_to_read):
-    test_ai_context.set_input("context", "The path of this file is the following and you can infer the name of the component from it:" + test_ai_context.get_output("file_contents")[i])
-    test_ai_context.set_input("question", prompt_info["parameters"]["question"] + "-File Content:" + test_ai_context.get_output("file_contents")[i])
-    try:
-        AskChatGpt().run_step(prompt_info, ai_context=test_ai_context)
-        docs[test_ai_context.get_output("file_names")[i]] = test_ai_context.get_output("chatgpt_response")
-    except Exception as e:
-        print("Error in generating documentation for file: " + test_ai_context.get_output("file_names")[i])
-        print(str(e))
-        continue
+def generate_docs(context,repo_name, folders, file_regex, branch):
 
-print(docs["src/App.tsx"])
+    if not repo_name or not folders or not file_regex or not branch:
+        raise ValueError("All parameters must have a non-empty value!")
+    # Create dictionary for repo_info
+    repo_info = {
+        "parameters": {
+            "repo_name": repo_name,
+            "folders": folders,
+            "file_regex": file_regex,
+            "branch": branch
+        },
+    }
 
-# Save the documentation in TestDocs folder
-for file_name, file_content in docs.items():
-    with open("TestDocs/" + file_name.replace("/", "_") + ".txt", "w") as f:
-        f.write(file_content)
+    # Create dictionary for prompt info
+    prompt_info = {
+        "parameters": {
+            "question": "Generate Markdown documentation for this code. This documentation is meant to summarize the purpose and technical details of this code file.Use headings to breakdown the documentation into the following sections: Summary: A one sentence summary of this component's functionality. Inputs: briefly describe the inputs and their purpose. Dependencies: briefly describe dependencies and their purpose. Outputs: briefly describe outputs and their purpose.",
+            "context": context,
+            "function": ""
+        },
+    }
+
+    # Create mock ai context
+    test_ai_context = MockAiContext()
+    test_ai_context.set_input("context", "")
+
+    # Get repo file names and contents into ai context
+    GitHubFileReader().run_step(repo_info, test_ai_context)
+    print(test_ai_context.get_output("file_names"))
+
+    num_files_to_read = len(test_ai_context.get_output("file_names"))
+    docs = {}
+
+    # Generate documentation for each file
+    for i in range(num_files_to_read):
+        test_ai_context.set_input("context", prompt_info["parameters"][context])
+        test_ai_context.set_input("question", prompt_info["parameters"]["question"] + "-File Content:" + test_ai_context.get_output("file_contents")[i])
+        try:
+            AskChatGpt().run_step(prompt_info, ai_context=test_ai_context)
+            docs[test_ai_context.get_output("file_names")[i]] = test_ai_context.get_output("chatgpt_response")
+        except Exception as e:
+            print("Error in generating documentation for file: " + test_ai_context.get_output("file_names")[i])
+            print(str(e))
+            continue
+    
+    # print(docs["src/App.tsx"]
+    return docs
+
+    # Save the documentation in TestDocs folder
+    for file_name, file_content in docs.items():
+        with open("TestDocs/" + file_name.replace("/", "_") + ".txt", "w") as f:
+            f.write(file_content)
--- DocApp/app.py
+++ DocApp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+import json
+import DocsGenerator 
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,6 +8,38 @@ def index():
     return "Hello, world!"
 
 
+
+@app.route("/generate_docs", methods=['POST'])
+def generate_docs_endpoint():
+    data = request.json
+
+    repo_name = data.get('repo_name', "") # iNBest-cloud/Telematica_AI_0723
+    folders = data.get('folders', "") # src
+    file_regex = data.get('file_regex', "") #.*.tsx
+    branch = data.get('branch', "") # develop
+
+    try:
+        docs = DocsGenerator.generate_docs(repo_name, folders, file_regex, branch)
+        return jsonify(docs)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400  # 400 is HTTP status code for Bad Request
+    
+@app.route("/check_regex_with_repo", methods=['POST'])
+def check_regex_with_repo_endpoint():
+    data = request.json
+    repo_name = data.get('repo_name')
+    folders = data.get('folders')
+    file_regex = data.get('file_regex')
+    branch = data.get('branch')
+
+    # Make sure all necessary parameters are provided
+    if not repo_name or not folders or not file_regex or not branch:
+        return jsonify({"error": "All parameters must be provided!"}), 400  # Bad Request
+
+    # Call the function and get the result
+    file_names = DocsGenerator.check_regex_with_repo(repo_name, folders, file_regex, branch)
+    
+    return jsonify({"file_names": file_names})
 
 
 if __name__ == "__main__":
